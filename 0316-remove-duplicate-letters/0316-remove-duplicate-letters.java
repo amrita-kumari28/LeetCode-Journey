58814a002abdb3a98fd387103efbda3ad572584e
Amrita Kumari <2305594@kiit.ac.in>
2026-07-19 08:44:27 +0530

    Time: 2 ms (86.89%), Space: 44 MB (26.85%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.java
@@ -0,0 +1,24 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] count= new int[26];
+        boolean[] visited=new boolean[26];
+        for(char ch:s.toCharArray()){
+            count[ch-'a']++;
+        }
+        Stack<Character> stack=new Stack<>();
+        for(char ch:s.toCharArray()){
+            count[ch-'a']--;
+            if(visited[ch-'a']) continue;
+            while(!stack.isEmpty() && ch<stack.peek() && count[stack.peek()-'a']>0){
+                visited[stack.pop()-'a']=false;
+            }
+            stack.push(ch);
+            visited[ch-'a']=true;
+        }
+        StringBuilder ans=new StringBuilder();
+        while(!stack.isEmpty()){
+            ans.append(stack.pop());
+        }
+        return ans.reverse().toString();
+    }
+}
