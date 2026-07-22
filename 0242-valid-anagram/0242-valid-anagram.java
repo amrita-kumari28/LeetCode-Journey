81063382a42228318901e77c47fe6e121e9be7a1
Amrita Kumari <2305594@kiit.ac.in>
2026-07-22 12:16:52 +0530

    Time: 2 ms (99.6%), Space: 44.6 MB (57.05%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int freq[]=new int[26];
+        for(char i: s.toCharArray()){
+            freq[i-'a']++;
+        }
+        for(char i: t.toCharArray()){
+            freq[i-'a']--;
+        }
+        for(int count:freq){
+            if(count!=0){
+                return false;
+            }
+        }
+        return true;
+    }
+}
