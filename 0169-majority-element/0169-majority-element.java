30a4244c947e1ceeee4b211ff8103e4255e439f4
Amrita Kumari <2305594@kiit.ac.in>
2026-08-15 12:33:19 +0530

    Time: 13 ms (32.09%), Space: 52.4 MB (95.16%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int num:nums){
+            map.put(num,map.getOrDefault(num,0)+1);
+        }
+        for(int n:nums){
+            if(map.get(n)>(nums.length/2)) return n;
+        }
+        return 0;
+    }
+}
