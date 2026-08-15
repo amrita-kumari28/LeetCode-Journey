56b4f8a8d8d4fd9628158b663c9639245ac3b7f2
Amrita Kumari <2305594@kiit.ac.in>
2026-08-15 23:08:31 +0530

    Time: 2 ms (88.92%), Space: 47.9 MB (43.49%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxreach=0;
+        for(int i=0;i<nums.length;i++){
+            if(i>maxreach) return false;
+            maxreach=Math.max(maxreach,nums[i]+i);
+            if(maxreach>=nums.length-1) return true;
+        }
+        return false;
+    }
+}
