d3d94cd333f608458fdaead136e1474b2837fc97
Amrita Kumari <2305594@kiit.ac.in>
2026-08-15 11:36:10 +0530

    Time: 1 ms (100%), Space: 133.6 MB (34.56%) - LeetHub

---
+++ 3702-longest-subsequence-with-non-zero-bitwise-xor/3702-longest-subsequence-with-non-zero-bitwise-xor.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int longestSubsequence(int[] nums) {
+        int xor=0;
+        for(int n:nums){
+            xor^=n;
+        }
+        if(xor != 0) return nums.length;
+        for(int num:nums){
+            if(num!=0) return nums.length-1;
+        }
+        return 0;
+    }
+}
