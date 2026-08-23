3106d5c2ceaf893c1d9ae9788a821f7d5f70a730
Amrita Kumari <2305594@kiit.ac.in>
2026-08-24 00:30:00 +0530

    Time: 9 ms (29.93%), Space: 47.1 MB (41.61%) - LeetHub

--- 1927-sum-game/1927-sum-game.java
+++ 1927-sum-game/1927-sum-game.java
@@ -1,5 +1,20 @@
 class Solution {
     public boolean sumGame(String num) {
-        
+        int leftsum=0;
+        int rightsum=0;
+        int qleft=0,qright=0;
+        for(int i=0;i<num.length()/2;i++){
+            if(num.charAt(i)=='?')
+            qleft++;
+            else
+            leftsum+=num.charAt(i)-'0';
+        }
+        for(int i=num.length()/2;i<num.length();i++){
+            if(num.charAt(i)=='?')
+            qright++;
+            else
+            rightsum+=num.charAt(i)-'0';
+        }
+        return 2*(leftsum-rightsum) != 9*(qright-qleft);
     }
 }
